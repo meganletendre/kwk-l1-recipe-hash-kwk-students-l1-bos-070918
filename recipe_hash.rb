7953c6209d51879f4b1761e8fbf3a24559932b12
meganletendre <megan.letendre@comcast.net>
2018-07-11 18:50:34 +0000

    automatically backed up by learn

--- recipe_hash.rb
+++ recipe_hash.rb
@@ -13,16 +13,17 @@
 #  - agave - 1/4 cup
 # All of the ingredients should be symbols and the amounts should be strings. Remember that symbols start with a colon and be sure to sub underscores for spaces.
 
-perfect_10_recipe = {almond flour - 2 cups
-gluten freewhole oats - 1 cup
-kosher salt - 1/2 tea spoon
-baking powder  - 1/2 tea spoon
-baking soda - 1/4 tea spoon
-#  - Xanthan Gum - 1/2 tea spoon
-#  - slivered almonds, lightly toasted - 1/4 cup
-#  - mini dark chocolate chips - 3/4 cup
-#  - olive oil - 1/2 cup
-#  - agave - 1/4 cup} 
+perfect_10_recipe = {
+  almond flour => 2 cups
+gluten freewhole oats => 1 cup
+kosher salt => 1/2 tea spoon
+baking powder  => 1/2 tea spoon
+baking soda => 1/4 tea spoon
+xanthan gum => 1/2 tea spoon
+slivered almonds, lightly toasted => 1/4 cup
+mini dark chocolate chips => 3/4 cup
+olive oil => 1/2 cup
+agave => 1/4 cup} 
 
 
 
